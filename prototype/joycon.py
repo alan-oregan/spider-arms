f6ad871ad39541d550a29ed48fdab8722b3d026f
Alan O'Regan <72257010+alan-oregan@users.noreply.github.com>
2022-10-16 18:05:35 +0100

    Update joycon.py to include a button and a longer delay for stability

--- prototype/joycon.py
+++ prototype/joycon.py
@@ -4,11 +4,9 @@ import time
 joycon_id = get_R_id()
 joycon = GyroTrackingJoyCon(*joycon_id)
 
-print(joycon.get_button_a)
-
 while True:
     # print("joycon pointer:  ", joycon.pointer)
     # print("joycon rotation: ", joycon.rotation)
     # print("joycon direction:", joycon.direction)
-    print(joycon.pointer, end="\r")
-    time.sleep(0.05)
+    print(f"Pointer = {joycon.pointer} A button pressed = {joycon.get_button_a()}", end="\r")
+    time.sleep(0.1)
